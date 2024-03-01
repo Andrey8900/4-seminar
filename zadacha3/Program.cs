05c9d6077f2366f0cccfa6a876a9043b5b1c5917
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-03-01 20:41:19 +0300

    3 задача

---
+++ zadacha3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая перевернёт одномерный массив
+//(первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] f = { 5, 4, 13, 9, 22 };
+        int invertedArray;
+        Console.Write("Исходный массив: ");
+        foreach (int number in f)
+        {
+            Console.Write(number + " ");
+        }
+
+        for (int i = 0; i < f.Length / 2; i++)
+        {
+            invertedArray = f[i];
+            f[i] = f[f.Length - i - 1];
+            f[f.Length - i - 1] = invertedArray;
+        }
+        Console.Write("\nПеревернутый массив: ");
+        foreach (int number in f)
+        {
+            Console.Write(number + " ");
+        }
+
+    }
+}
+
