38d6e754700cf568c5d828a4ae88d265f6bf3797
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-03-01 20:37:21 +0300

    задача2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте массив заполненный случайными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+using System;
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+
+        int[] numbers = new int[10];
+
+        int evenCount = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rnd.Next(100, 1000);
+            Console.Write(numbers[i] + " ");
+
+
+            if (numbers[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+            Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
+
+        }
+
+    }
+}
